d4680881490eb8bebf3d688c93917b5dbe3aa056
b14ck4nge1 <110855423+b14ck4nge1@users.noreply.github.com>
2022-10-08 15:57:40 +0330

    Update A.py

--- A.py
+++ A.py
@@ -1,1 +1,38 @@
 
+def n():
+    global num
+    num=int(input("Enter Number of candidates: "))
+    if 1<=num and num<=100:
+        na()
+    else:
+        print("Enter Number beetwen 1-100")
+        n()
+
+def na():
+    name = input("""Enter Nominator names: (separate with ",") """)
+    l = name.split(",")
+    if num==len(l):
+        print(l)
+        qad()
+    else:
+        print("tedad eshtebah ast")
+        na()
+
+def qad():
+    q=input("""Enter Nominator hights: (separate with "|") """)
+    if type(q)==str:
+        l2 = q.split("|")
+        if len(l2)==num:
+            la=l2.copy()
+        else:
+            print("tedad eshtebah ast")
+    else:
+        print("Enter Number beetwen 1-300")
+        qad()
+    la = list(map(int, la))
+    for i in range(len(la)):
+        for j in range(i + 1, len(la)):
+
+            if la[i] > la[j]:
+                la[i], la[j] = la[j], la[i]
+n()
